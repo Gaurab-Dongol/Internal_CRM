81667b12e832e1ce0d4acce869df2fdf6bc12881
Chintan suthar <chintan.suthar777@gmail.com>
2021-07-21 14:05:00 +1000

    design changes

--- fetch.php
+++ fetch.php
@@ -4,18 +4,15 @@
 
 $connect = new PDO("mysql:host=localhost;dbname=Internal_CRM", "root", "root");
 
-if($_POST["query"] != '')
-{
+if ($_POST["query"] != '') {
 	$search_array = explode(",", $_POST["query"]);
 	$search_text = "'" . implode("', '", $search_array) . "'";
 	$query = "
 	SELECT * FROM OfficeHQ 
-	WHERE Consultant IN (".$search_text.") 
+	WHERE Consultant IN (" . $search_text . ") 
 	ORDER BY Consultant DESC
 	";
-}
-else
-{
+} else {
 	$query = "SELECT * FROM OfficeHQ ORDER BY Consultant DESC";
 }
 
@@ -29,27 +26,26 @@ $total_row = $statement->rowCount();
 
 $output = '';
 
-if($total_row > 0)
-{
-	foreach($result as $row)
-	{
+if ($total_row > 0) {
+	foreach ($result as $row) {
 		$output .= '
         <tr>
-			<td>'.$row["Client_Name"].'</td>
-			<td>'.$row["Email_Id"].'</td>
-			<td>'.$row["Contact"].'</td>
-			<td>'.$row["Enquiry"].'</td>
-            <td>'.$row["Notes"].'</td>
-            <td>'.$row["Status"].'</td>
-            <td>'.$row["Current_Visa"].'</td>
-            <td>'.$row["Appointment"].'</td>
-            <td>'.$row["MM_Update"].'</td>
+			<td>' . $row["Client_Name"] . '</td>
+			<td>' . $row["Email_Id"] . '</td>
+			<td>' . $row["Contact"] . '</td>
+			<td>
+			' . $row["Enquiry"] . '
+			
+			</td>
+            <td>' . $row["Notes"] . '</td>
+            <td>' . $row["Status"] . '</td>
+            <td>' . $row["Current_Visa"] . '</td>
+            <td>' . $row["Appointment"] . '</td>
+            <td>' . $row["MM_Update"] . '</td>
 		</tr>
 		';
 	}
-}
-else
-{
+} else {
 	$output .= '
 	<tr>
 		<td colspan="5" align="center">No Data Found</td>
@@ -58,6 +54,3 @@ else
 }
 
 echo $output;
-
-
-?>
