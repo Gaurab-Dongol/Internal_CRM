b732d45c243fbf187e2dc57fa5212d99a232041e
Gaurab Dongol <gdongol@192.168.1.119>
2021-07-21 13:01:16 +1000

    user

--- config.php
+++ config.php
@@ -1,12 +1,26 @@
 <?php 
 	//session_start(); // we will use it later  to store logged in user information such as username
-	define("HOST","localhost");
-	define("DB_USER","root");
-	define("DB_PASS","root");
-	define("DB_NAME","Internal_CRM");
-	$conn = mysqli_connect(HOST,DB_USER,DB_PASS,DB_NAME);
-	if(!$conn)
+	//define("HOST","localhost");
+	//define("DB_USER","root");
+	//define("DB_PASS","root");
+	//define("DB_NAME","Internal_CRM");
+	//$conn = mysqli_connect(HOST,DB_USER,DB_PASS,DB_NAME);
+	//if(!$conn)
+	//{
+	//	die(mysqli_error());
+	//}
+
+	$connect = new PDO("mysql:host=localhost;dbname=Internal_CRM", "root", "root");
+
+	function getUserAccessRoleByID($id)
 	{
-		die(mysqli_error());
+		global $connnect;
+		
+		$query = "select RoleId from login where RoleId = ".$id;
+	
+		$rs = mysqli_query($conn,$query);
+        $row = mysqli_fetch_assoc($rs);
+		
+		return $row['RoleId'];
 	}
 ?>
---
+++ header.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title Page-->
+    <title>Internal CRM</title>
+
+    <link href="css/bootstrap-select.min.css" rel="stylesheet" />
+    <script src="js/bootstrap-select.min.js"></script>
+
+    <!-- Fontfaces CSS-->
+    <link href="css/font-face.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/theme.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+
+
+    <body class="animsition">
+        <div class="page-wrapper">
+            <!-- HEADER DESKTOP-->
+            <header class="header-desktop3 d-none d-lg-block">
+                <div class="section__content section__content--p35">
+                    <div class="header3-wrap">
+                        <div class="header__logo">
+                            <a href="index.php">
+                                <h2>Internal CRM</h2>
+                            </a>
+                        </div>
+
+                        <div class="header__navbar">
+                            <ul class="list-unstyled">
+                                <li class="has-sub">
+                                    <a href="#">
+                                        <i class="fas fa-tachometer-alt"></i>Leads
+                                        <span class="bot-line"></span>
+                                    </a>
+                                    <ul class="header3-sub-list list-unstyled">
+                                        <li>
+                                            <a href="Social_media.php">Social Media</a>
+                                        </li>
+                                        <li>
+                                            <a href="task.php">Task</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="header__tool">
+                            <div class="account-wrap">
+                                <div class="account-item account-item--style2 clearfix js-item-menu">
+                                    <div class="content">
+                                        <a class="js-acc-btn" href="#">chintan suthar</a>
+                                    </div>
+                                    <div class="account-dropdown js-dropdown">
+                                        <div class="info clearfix">
+
+                                            <div class="content">
+                                                <h5 class="name">
+                                                    <a href="#">chintan suthar</a>
+                                                </h5>
+                                                <span class="email">chintan@visas.com</span>
+                                            </div>
+                                        </div>
+                                        <div class="account-dropdown__body">
+                                            <div class="account-dropdown__item">
+                                                <a href="#">
+                                                    <i class="zmdi zmdi-account"></i>Account</a>
+                                            </div>
+                                            <div class="account-dropdown__item">
+                                                <a href="#">
+                                                    <i class="zmdi zmdi-settings"></i>Setting</a>
+                                            </div>
+
+                                        </div>
+                                        <div class="account-dropdown__footer">
+                                            <a href="#">
+                                                <i class="zmdi zmdi-power"></i>Logout</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- END HEADER DESKTOP-->
+
+            <!-- WELCOME-->
+            <br>
+            <section class="welcome p-t-10">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="title-4">Client Details
+                            </h1>
+                            <hr class="line-seprate">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- END WELCOME-->
