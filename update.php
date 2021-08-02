518eafb9049111c4e34be9d5325fc96cfed2ce2f
Chintan suthar <chintan.suthar777@gmail.com>
2021-08-02 15:29:54 +1000

    edit modal form

--- update.php
+++ update.php
@@ -1,10 +1,10 @@
 <?php
-	require_once 'config.php';
-	
-	if($_GET['Id'] != ""){
-		$id = $_GET['Id'];
-		
-		$conn->query("UPDATE OfficeHQ SET status = 'Done' WHERE Id = $d") or die(mysqli_errno());
-		header('location: index.php');
-	}
-?>
+require_once 'config.php';
+
+
+if ($_GET['Id'] != "") {
+	$id = $_GET['Id'];
+
+	$conn->query("UPDATE OfficeHQ SET status = 'Done' WHERE Id = $d") or die(mysqli_errno());
+	header('location: index.php');
+}
